63208083f08cc3154a6e2585e7854943dfffc7ed
Puja Dey <pujadey48@gmail.com>
2022-11-27 03:24:51 +0600

    category fetching added

--- index.js
+++ index.js
@@ -24,14 +24,17 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 function verifyJWT(req, res, next){
     const authHeader = req.headers.authorization;
+    console.log('authHeader', authHeader);
 
     if(!authHeader){
+        console.log('auth header not present');
         return res.status(401).send({message: 'unauthorized access'});
     }
     const token = authHeader.split(' ')[1];
 
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded){
         if(err){
+            console.log('Token verification falied');
             return res.status(403).send({message: 'Forbidden access'});
         }
         req.decoded = decoded;
@@ -42,25 +45,79 @@ function verifyJWT(req, res, next){
 async function run(){
 try{
     const userCollection = client.db('swapClothes').collection('user');
+    const categoriesCollection = client.db('swapClothes').collection('categories');
+    const productCollection = client.db('swapClothes').collection('products');
 
     app.post('/jwt', async (req, res) =>{
-        const user = req.body;
+        let user = req.body;
         const query = { uid: user.uid};
         const dbUser = await userCollection.findOne(query);
         if(!dbUser){
             await userCollection.insertOne(user);
+        } else {
+            user = dbUser;
         }
-        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d'})
-        console.log({user, token});
-        res.send({token})
+        role = user.role;
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d'});
+        console.log({user, token, role});
+        res.send({role, token})
     })
 
-    // app.post('/services', verifyJWT, async (req, res) => {
-    //     const service = req.body;
-    //     const result = await serviceCollection.insertOne(service);
-    //     console.log("result", result);
-    //     res.send(result);
-    // });
+    app.get('/categories', async (req, res) => {
+        const query = {};
+        const categories = await categoriesCollection.find(query).toArray();
+        console.log(categories);
+        res.send(categories);
+    })
+
+    app.get('/users/admin/:uid', async (req, res) => {
+        const admin = req.params.uid;
+        const query = { uid : admin}
+        const user = await userCollection.findOne(query);
+        console.log(user);
+        res.send({ isAdmin: user?.role === 'admin' });
+    })
+
+    app.post('/product', verifyJWT, async (req, res) => {
+        const product = req.body;
+        product.createdBy = req.decoded.uid;
+        product.timestamp = Date.now();
+        product.reported = false;
+        product.status = "available";
+        const result = await productCollection.insertOne(product);
+        console.log("result", result);
+        res.send(result);
+    });
+
+    app.get('/myproducts', verifyJWT, async (req, res) => {
+        const query = { createdBy : req.decoded.uid}
+        const products = await productCollection.find(query).toArray();
+        console.log("result", products);
+        res.send(products);
+    });
+
+    app.delete('/product/:id', verifyJWT, async (req, res) => {
+        const id = req.params.id;
+
+        const uid = req.decoded.uid;
+        let query = { uid}
+        const user = await userCollection.findOne(query);
+
+        query = { _id: ObjectId(id) };
+        const product = await productCollection.findOne(query);
+
+        if(!user || !product){
+            res.send({status: false, message: "product not deleted"});
+        }
+        if(user.role == 'admin' || user.uid === product.createdBy){
+            query = { _id: ObjectId(id) };
+            const result = await productCollection.deleteOne(query);
+            console.log({query, result});
+            res.send(result);
+        } else {
+            res.send({status: false, message: "product not deleted"});
+        }
+    })
 
 }
 finally{
